0b094f75a5f4ca393a1d4c6b228951de0553bf06
Takos22 <tora.akos@gmail.com>
2020-07-28 18:05:10 +0200

    Update example.py Add comments

--- example.py
+++ example.py
@@ -2,22 +2,28 @@ import requests
 
 URL = "http://127.0.0.1:5000"
 
+# Random question
 r = requests.get(URL + "/question")
 print(r.json())
 
+# Answer to that question
 r = requests.get(URL + f"/answer/{r.json()['id']}")
 print(r.json())
 
+# Random question from the "Tech" category
 r = requests.get(URL + "/question/1")
 print(r.json())
 
+# Answer to that question
 r = requests.get(URL + f"/answer/{r.json()['id']}")
 print(r.json())
 
-r = requests.get(URL + "/question/4")
+# 404 error because there's no category 0
+r = requests.get(URL + "/question/0")
 print(r.status_code)
 print(r.json())
 
-r = requests.get(URL + "/answer/5")
+# 404 error because there's no question 0
+r = requests.get(URL + "/answer/0")
 print(r.status_code)
 print(r.json())
