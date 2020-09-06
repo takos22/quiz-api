64bd9b0c74b91b5208b9ab60d9bf9638bc02458d
Takos22 <tora.akos@gmail.com>
2020-09-06 18:12:00 +0200

    Add type hinting

--- api/resources.py
+++ api/resources.py
@@ -1,14 +1,15 @@
 from flask_restful import Resource, abort
 from sqlalchemy.sql.expression import func
 import json
+from typing import Optional
+
 from api import models
 
 
 class Question(Resource):
-    def get(self, category_id: int = None):
-        print(category_id)
+    def get(self, category_id: Optional[int] = None):
         if category_id is not None:
-            question = (
+            question: models.Question = (
                 models.Question.query.filter_by(category_id=category_id)
                 .order_by(func.random())
                 .first_or_404(
@@ -17,11 +18,13 @@ class Question(Resource):
                 )
             )
         else:
-            question = models.Question.query.order_by(func.random()).first()
+            question: models.Question = models.Question.query.order_by(func.random()).first()
         return {"id": question.id, "question": question.question, "choices": json.loads(question.choices)}
 
 
 class Answer(Resource):
     def get(self, question_id: int):
-        question = models.Question.query.filter_by(id=question_id).first_or_404(description=f"Provided question_id ({question_id}) doesn't exist.")
+        question: models.Question = models.Question.query.filter_by(id=question_id).first_or_404(
+            description=f"Provided question_id ({question_id}) doesn't exist."
+        )
         return {"id": question.id, "answer": question.answer}
